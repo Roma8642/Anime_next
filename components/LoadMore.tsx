e6256a14ce77a8bebfb2210f9c244dea425c42b7
Roma8642 <141335182+Roma8642@users.noreply.github.com>
2023-12-08 23:21:46 +0200

    started

--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -1,10 +1,23 @@
+"use client";
 import Image from "next/image";
+import {useInView} from "react-intersection-observer";
+import {useEffect, useState} from "react";
+import {fetchAnime} from "@/app/action";
 
 function LoadMore() {
+    const{ref, inView}=useInView()
+    const [data, setData] = useState([])
+    useEffect(()=>{
+        if(inView){
+        fetchAnime(2)  .then((res)=>{
+            // setData([...data, ...res])
+        })
+        }
+    },[inView])
   return (
     <>
       <section className="flex justify-center items-center w-full">
-        <div>
+        <div ref={ref}>
           <Image
             src="./spinner.svg"
             alt="spinner"
