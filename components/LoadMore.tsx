3c8b0fc1789fe563bc77f33ea568cb67293a1302
Roma8642 <141335182+Roma8642@users.noreply.github.com>
2023-12-09 15:50:50 +0200

    finish

--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -1,34 +1,61 @@
 "use client";
+
 import Image from "next/image";
-import {useInView} from "react-intersection-observer";
-import {useEffect, useState} from "react";
-import {fetchAnime} from "@/app/action";
+import { useInView } from "react-intersection-observer";
+import { useEffect, useState } from "react";
+
+import { fetchAnime } from "@/app/action";
+
+let page = 2;
+
+export type AnimeCard = JSX.Element;
 
 function LoadMore() {
-    const{ref, inView}=useInView()
-    const [data, setData] = useState([])
-    useEffect(()=>{
-        if(inView){
-        fetchAnime(2)  .then((res)=>{
-            // setData([...data, ...res])
-        })
+    const { ref, inView } = useInView();
+
+    const [data, setData] = useState<AnimeCard[]>([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        if (inView) {
+            setIsLoading(true);
+            // Add a delay of 500 milliseconds
+            const delay = 500;
+
+            const timeoutId = setTimeout(() => {
+                fetchAnime(page).then((res) => {
+                    setData([...data, ...res]);
+                    page++;
+                });
+
+                setIsLoading(false);
+            }, delay);
+
+            // Clear the timeout if the component is unmounted or inView becomes false
+            return () => clearTimeout(timeoutId);
         }
-    },[inView])
-  return (
-    <>
-      <section className="flex justify-center items-center w-full">
-        <div ref={ref}>
-          <Image
-            src="./spinner.svg"
-            alt="spinner"
-            width={56}
-            height={56}
-            className="object-contain"
-          />
-        </div>
-      </section>
-    </>
-  );
+    }, [inView, data, isLoading]);
+
+    return (
+        <>
+            <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
+                {data}
+            </section>
+            <section className="flex justify-center items-center w-full">
+                <div ref={ref}>
+                    {inView && isLoading && (
+                        <Image
+                            src="./spinner.svg"
+                            alt="spinner"
+                            width={56}
+                            height={56}
+                            className="object-contain"
+                        />
+                    )}
+                </div>
+            </section>
+        </>
+    );
 }
 
 export default LoadMore;
